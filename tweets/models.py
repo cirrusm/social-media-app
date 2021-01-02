d3ae3cdafa0409cb45307742e85cb63c5d3dad14
Cirrus Mokhtari <Cirrus1994@gmail.com>
2021-01-01 21:43:08 -0800

    added Like Model

--- tweets/models.py
+++ tweets/models.py
@@ -3,11 +3,17 @@ from django.conf import settings
 
 User = settings.AUTH_USER_MODEL
 
+class TweetLike(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    tweet = models.ForeignKey('Tweet', on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
 class Tweet(models.Model):
     user = models.ForeignKey(User, on_delete = models.CASCADE) #User can have many tweets but tweet only has one user
     content = models.TextField(blank=True, null=True)
     image = models.FileField(upload_to='images/', blank=True, null=True)
-    # Create your models here.
+    likes = models.ManyToManyField(User, related_name = 'tweet_user', blank = True)
+    timestamp = models.DateTimeField(auto_now_add=True)
     class Meta:
         ordering = ['-id']
 
